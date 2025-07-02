e07ea07acfe4e7046e24eb99e2dedb57e0a80366
Subham-Kumar-Padhy <subhamkumarpadhy14@gmail.com>
2025-07-02 22:41:29 +0530

    Set the project setup file

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements()->List[str]:
+    """
+    This function will return list of requirements
+    """
+    requirement_list:List[str] = []
+    
+    try:
+        with open('requirements.txt', 'r') as file:
+            lines = file.readlines()
+            for line in lines:
+                requirement = line.strip()
+                if requirement and requirement != '-e .':
+                    requirement_list.append(requirement)
+    except FileNotFoundError:
+        print("requirements.txt file not found.")
+
+    
+        
+    return requirement_list
+print(get_requirements())
+
+setup(
+    name="AI-TRIP-PLANNER",
+    version="0.0.1",
+    author="Subham Kumar Padhy",
+    author_email="example@gmail.com",
+    packages = find_packages(),
+    install_requires=get_requirements()
+)
